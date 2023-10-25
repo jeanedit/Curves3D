03723d4c869eb33b9b75729d9077ab5fb02db40a
Valeriy <3bhdchannel@gmail.com>
2023-10-25 19:36:49 +0500

    Divided Point and Vector

--- 3D_Curves/src/3DCurves_dll_interface.cpp
+++ 3D_Curves/src/3DCurves_dll_interface.cpp
@@ -14,11 +14,11 @@ Point3D Circle::GetPoint(double t) const {
 }
 
 Vector3D Circle::GetDerivative(double t) const {
-	Vector3D deriviative;
-	deriviative.x = -m_Radius * sin(t);
-	deriviative.y = m_Radius * cos(t);
-	deriviative.z = 0.0;
-	return deriviative;
+	Vector3D derivative;
+	derivative.x = -m_Radius * sin(t);
+	derivative.y = m_Radius * cos(t);
+	derivative.z = 0.0;
+	return derivative;
 }
 
 Point3D Ellipse::GetPoint(double t) const {
@@ -31,11 +31,11 @@ Point3D Ellipse::GetPoint(double t) const {
 
 
 Vector3D Ellipse::GetDerivative(double t) const {
-	Vector3D deriviative;
-	deriviative.x = -m_SemiMajorAxis * sin(t);
-	deriviative.y = m_SemiMinorAxis * cos(t);
-	deriviative.z = 0.0;
-	return deriviative;
+	Vector3D derivative;
+	derivative.x = -m_SemiMajorAxis * sin(t);
+	derivative.y = m_SemiMinorAxis * cos(t);
+	derivative.z = 0.0;
+	return derivative;
 }
 
 Point3D Helix3D::GetPoint(double t) const {
@@ -47,9 +47,9 @@ Point3D Helix3D::GetPoint(double t) const {
 }
 
 Vector3D Helix3D::GetDerivative(double t) const {
-	Vector3D deriviative;
-	deriviative.x = -m_Radius * sin(t);
-	deriviative.y = m_Radius * cos(t);
-	deriviative.z = m_Step / (2 * PI);
-	return deriviative;
+	Vector3D derivative;
+	derivative.x = -m_Radius * sin(t);
+	derivative.y = m_Radius * cos(t);
+	derivative.z = m_Step / (2 * PI);
+	return derivative;
 }
