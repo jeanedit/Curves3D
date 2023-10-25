d5c157672d7d9d1fac231ce20fa84b5d52af3daf
Valeriy <3bhdchannel@gmail.com>
2023-10-25 18:46:22 +0500

    Third commit

--- 3D_Curves/src/3DCurves_dll_interface.cpp
+++ 3D_Curves/src/3DCurves_dll_interface.cpp
@@ -3,11 +3,6 @@
 
 #include <math.h>
 
-std::ostream& operator << (std::ostream& os, Point3D point) {
-	os << "(" << point.x << ", " << point.y << ", " << point.z << ")";
-	return os;
-}
-
 
 
 Point3D Circle::GetPoint(double t) const {
@@ -58,63 +53,3 @@ Point3D Helix3D::GetDerivative(double t) const {
 	deriviative.z = m_Step / (2 * PI);
 	return deriviative;
 }
-
-
-
-
-//struct curvesCurve3D {
-//	curves::Curve3D curve;
-//};
-//
-//struct curvesCircle {
-//	curves::Circle circle;
-//};
-//
-//struct curvesEllipse {
-//	curves::Ellipse ellipse;
-//};
-//
-//struct curvesHelix3D {
-//	curves::Helix3D helix3d;
-//};
-//
-//
-//// Functions to create and destroy a Circle
-//curvesCurve3D* CreateCircle(double radius) {
-//	curves::Circle circle(radius);
-//	return new curvesCurve3D{circle};
-//}
-//
-//// Function to create an Ellipse
-//curvesCurve3D* CreateEllipse(double a, double b) {
-//	curves::Ellipse ellipse (a,b);
-//	return new curvesCurve3D{ ellipse };
-//}
-//
-//// Function to create a Helix
-//curvesCurve3D* CreateHelix(double radius, double step) {
-//	curves::Helix3D helix3d(radius,step);
-//	return new curvesCurve3D{ helix3d };
-//}
-//double GetRadius(curvesCircle* circle) {
-//	return circle->circle.GetRadius();
-//}
-//// Function to destroy a Curve3D object
-//void DestroyCurve(curvesCurve3D* curve) {
-//	delete curve;
-//}
-//
-//// Function to get the point
-//curvesPoint3D GetPoint(curvesCurve3D* curve, double t) {
-//	curves::Point3D val = curve->curve.GetPoint(t);
-//	curvesPoint3D& dll_val = reinterpret_cast<curvesPoint3D&>(val);
-//	return dll_val;
-//}
-//
-//// Function to get the derivative
-//curvesPoint3D GetDerivative(curvesCurve3D* curve, double t) {
-//	curves::Point3D val = curve->curve.GetDerivative(t);
-//	curvesPoint3D& dll_val = reinterpret_cast<curvesPoint3D&>(val);
-//	return dll_val;
-//}
-
--- 3D_Curves/src/3DCurves_dll_interface.h
+++ 3D_Curves/src/3DCurves_dll_interface.h
@@ -7,22 +7,19 @@
 #endif
 
 #define CURVES3D_EXPORT extern "C" CURVES3D_API
-#include <iostream>
-static const double PI = 3.14159265359;
 
+static const double PI = 3.14159265359;
 
 
 struct Point3D {
 	double x, y, z;
 };
 
-CURVES3D_API std::ostream& operator<<(std::ostream& os, Point3D point);
-
 class CURVES3D_API Curve3D {
 public:
 	virtual Point3D GetPoint(double t) const = 0;
 	virtual Point3D GetDerivative(double t) const = 0;
-	virtual ~Curve3D() {}
+	virtual ~Curve3D() { }
 };
 
 
@@ -67,20 +64,4 @@ private:
 	double m_Step;
 };
 
-
-
-//struct curvesCurve3D;
-//struct curvesCircle;
-//struct curvesEllipse;
-//struct curvesHelix3D;
-//
-//
-//
-//CURVES3D_EXPORT curvesCurve3D* CreateCircle(double radius);
-//CURVES3D_EXPORT curvesCurve3D* CreateEllipse(double a, double b);
-//CURVES3D_EXPORT curvesCurve3D* CreateHelix(double radius, double step);
-//CURVES3D_EXPORT double GetRadius(curvesCircle* circle);
-//CURVES3D_EXPORT void DestroyCurve(curvesCurve3D* curve);
-//CURVES3D_EXPORT curvesPoint3D GetPoint(curvesCurve3D* curve, double t);
-//CURVES3D_EXPORT curvesPoint3D GetDerivative(curvesCurve3D* curve, double t);
 
